66a9e5ed67f418756cf41da13041476ecf40954f
hemalchevli <hemal_chevli@yahoo.com>
2015-12-01 16:50:04 +0530

    Tweets based on serial input

---
+++ CNCTweet/CNCTweet.ino
@@ -0,0 +1,93 @@
+/*
+ *  This sketch sends data via HTTP GET requests to IFTTT maker channel
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "maker.ifttt.com";
+const char* privateKey = "....................";
+void Laser(int i);
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void loop() {
+  if(Serial.available()){
+    char c = Serial.read();
+    if(c == 'a'){  
+      Laser(1);
+    }
+    if(c == 'b'){
+      Laser(0);
+    }
+  }
+}
+
+void Laser(int i){
+  String url;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  if(i == 1){
+  url = "/trigger/LaserOn/with/key/";
+  }
+  else if(i == 0){
+    url = "/trigger/LaserOff/with/key/";
+    }
+  url += privateKey;
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+ }
